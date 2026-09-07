527c31ad7a4986875458e70f8c3c7ee391a811fd
Бойбородов Илья <fflash08@mail.ru>
2026-09-07 12:37:33 +0300

    feat(posts): Создать модель поста и зарегистрировать в админке.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Текст поста")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ['-created_at']
