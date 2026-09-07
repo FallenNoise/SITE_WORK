78343ec8ac79a529ebd2a24a5b7081a7937c7e5b
Бойбородов Илья <fflash08@mail.ru>
2026-09-07 12:41:25 +0300

    feat(home-page): Добавить функционал отображения главной страницы.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
-# Create your views here.
+
+def home_page(request):
+    posts = Post.objects.all()[:3]
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    date_filter = request.GET.get('date')
+    if date_filter:
+        posts = posts.filter(created_at__date=date_filter)
+    return render(request, 'blog/post_list.html', {'posts': posts})
