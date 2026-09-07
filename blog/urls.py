d3338241543cab98defe7bcdb7a26b5c2ee3937a
Бойбородов Илья <fflash08@mail.ru>
2026-09-07 13:03:17 +0300

    refactor(posts/add): Оптимизировать вьюшку добавления поста.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('posts/add/', views.post_add, name='post_add'),
-
+    path('post/add/', views.PostCreateView.as_view(), name='post_add'),
 ]
